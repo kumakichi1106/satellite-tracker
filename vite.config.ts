6e4c661b2c745332f2c0193b1275211cf755358f
kumakichi <kosuke.takano1106@gmail.com>
2026-05-17 22:58:38 +0900

    add:githubpagesの設定

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/satellite-tracker/',
   plugins: [react()],
+  worker: {
+    format: 'es',
+  },
+
+  build: {
+
+    target: 'es2022', 
+  },
 });
