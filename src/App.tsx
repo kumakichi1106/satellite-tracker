edb598350833350f8c98c46ced1936ca8494b199
kumakichi <kosuke.takano1106@gmail.com>
2026-05-20 15:25:39 +0900

    create:地球コンポネントの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Earth } from './components/earth/earth';
 import { SatelliteContainer } from './components/satellite';
 
 export function App() {
   return (
     <main className="app">
       <h1>Satellite Tracker</h1>
-      <SatelliteContainer />
+      {/* <SatelliteContainer /> */}
+      <Earth />
     </main>
   );
 }
--- src/components/earth/earth.tsx
+++ src/components/earth/earth.tsx
@@ -1,3 +1,22 @@
+import * as THREE from 'three';
+import { Canvas, useLoader } from "@react-three/fiber";
+import { CameraControls } from "@react-three/drei";
+
 export function Earth() {
-  return null;
+  const map = useLoader(THREE.TextureLoader, "./assets/earthmap.jpg");
+
+  return (
+    <div className="h-screen w-full">
+      <Canvas className="h-full w-full">
+        <mesh>
+          <sphereGeometry />
+          <meshPhongMaterial map={map} />
+        </mesh>
+        <ambientLight intensity={1} />
+        <CameraControls minDistance={1} maxDistance={100} />
+      </Canvas>
+
+    </div>
+  );
 }
+
